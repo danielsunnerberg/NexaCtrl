d2500c02f2c79d9052dfc0ffa6341d0204c08427
Daniel Sunnerberg <daniel.sunnerberg@gmail.com>
2015-12-06 14:44:54 +0100

    Removed unused method

--- firmware/NexaCtrl.cpp
+++ firmware/NexaCtrl.cpp
@@ -227,26 +227,3 @@ unsigned long power2(int power){    //gives 2 to the (power)
     }
     return integer;
 }
-
-unsigned long htoi (const char *ptr)
-{
-    unsigned long value = 0;
-    char ch = *ptr;
-
-    while (ch == ' ' || ch == '\t') {
-        ch = *(++ptr);
-    }
-
-    for (;;) {
-        if (ch >= '0' && ch <= '9') {
-            value = (value << 4) + (ch - '0');
-        } else if (ch >= 'A' && ch <= 'F') {
-            value = (value << 4) + (ch - 'A' + 10);
-        } else if (ch >= 'a' && ch <= 'f') {
-            value = (value << 4) + (ch - 'a' + 10);
-        } else {
-            return value;
-        }
-        ch = *(++ptr);
-    }
-}
